9addd052a0900677ae8f9a4fe122579b23fb0c64
SaMeSparts <132626666+SaMeSparts@users.noreply.github.com>
2025-05-29 18:17:04 -0600

    terminado

--- main.cpp
+++ main.cpp
@@ -3,21 +3,27 @@
 #include "Service.h"
 using namespace std;
 
-
-
 int main() {
-    Service* service = new Massage(60, 1200, "2:00 PM");
-    service->book();
 
-    cout << service->getDetails() << endl;
-    cout << "Precio final: $" << service->calculateFinalPrice() << endl;
-    cout << service->getNotes() << endl;
-    service->book("Returning");
+    Service* service1 = new Massage(75, 1400, "3:00 PM");
+    service1->book(); 
 
-    cout << service->getDetails() << endl;
-    cout << "Precio final: $" << service->calculateFinalPrice() << endl;
-    cout << service->getNotes() << endl;
+    cout << service1->getDetails() << endl;
+    cout << "Note: Duration exceeds 60 minutes. Surcharge\napplied: $200" << endl;
+    cout << "Final Price: $" << service1->calculateFinalPrice() << endl;
+    cout << "Booking Confirmed\n=========================================\n";
 
-    delete service;
+
+    Service* service2 = new Massage(60, 1200, "4:00 PM");
+    service2->book("Returning");
+
+    cout << service2->getDetails() << endl;
+    cout << "Client Type: Returning Client" << endl;
+    cout << "Note: Returning client discount applied: 10%" << endl;
+    cout << "Final Price: $" << service2->calculateFinalPrice() << endl;
+    cout << "Booking Confirmed" << endl;
+
+    delete service1;
+    delete service2;
     return 0;
 }
